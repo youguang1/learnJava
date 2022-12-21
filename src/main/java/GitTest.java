ee8d34e1f3b838c60f52a7d58aa54966127980ec
youguang <youguang_1@163.com>
2022-12-22 01:08:30 +0800

    第三次用IDEA提交

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -1,6 +1,8 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello github1!");
+        System.out.println("hello github2!");
         System.out.println("hello github!");
     }
 }
